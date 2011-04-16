8f544234ddc2208aa43d7bf445db44429d8888dc
anefian <anefian@1395e39a-8119-0410-9602-abf82d98ffd9>
2011-04-16 03:55:29 +0000

    deleted build.sh
    git-svn-id: https://babelfish.arc.nasa.gov/svn/stereopipeline/sandbox/lima@1528 1395e39a-8119-0410-9602-abf82d98ffd9

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-g++ -arch i386 -fopenmp coregister.cc tracks.cc match.cc io.cc display.cc weights.cc featuresLOLA.cc -o coregister -I. -I/opt/local/include  -I /Users/anefian/opencv/cv/include -I /Users/anefian/opencv/cvaux/include -I /Users/anefian/opencv/cxcore/include -I /Users/anefian/opencv/otherlibs/highgui  -I/Users/anefian/projects/VisionWorkbench/build/include -L/opt/local/lib -L/Users/anefian/projects/VisionWorkbench/build/lib -lvwCore -lvwMath -lvwImage -lboost_thread-mt -lboost_program_options-mt -lboost_filesystem-mt -lboost_system-mt -lvwFileIO -lvwCartography -lvwPhotometry -framework VecLib
-
-#g++ -arch i386 -fopenmp lidar2dem.cc tracks.cc match.cc io.cc display.cc weights.cc featuresLOLA.cc -o lidar2dem -I. -I/opt/local/include  -I /Users/anefian/opencv/cv/include -I /Users/anefian/opencv/cvaux/include -I /Users/anefian/opencv/cxcore/include -I /Users/anefian/opencv/otherlibs/highgui  -I/Users/anefian/projects/VisionWorkbench/build/include -L/opt/local/lib -L/Users/anefian/projects/VisionWorkbench/build/lib -lvwCore -lvwMath -lvwImage -lboost_thread-mt -lboost_program_options-mt -lboost_filesystem-mt -lboost_system-mt -lvwFileIO -lvwCartography -lvwPhotometry  -framework VecLib
-
