83a54ede21a6caa2b09c6282f602060699430fd7
rbeyer <rbeyer@1395e39a-8119-0410-9602-abf82d98ffd9>
2012-05-15 17:18:38 +0000

    Created a test for ComputeDEMTranslation().
    git-svn-id: https://babelfish.arc.nasa.gov/svn/stereopipeline/sandbox/lima@1766 1395e39a-8119-0410-9602-abf82d98ffd9

---
+++ tests/Test_icp.cc
@@ -0,0 +1,31 @@
+#include <boost/filesystem.hpp>
+
+#include "../icp.h"
+#include "gtest/gtest.h"
+
+namespace fs = boost::filesystem;
+
+using namespace std;
+
+TEST( ComputeDEMTranslation, works ){
+  vector<Vector3> features;
+  features.push_back( Vector3(1,1,1) );
+  features.push_back( Vector3(2,3,4) );
+  features.push_back( Vector3(21,31,41) );
+
+  vector<Vector3> match;
+  match.push_back( Vector3(0,0,0) );
+  match.push_back( Vector3(1,2,3) );
+  match.push_back( Vector3(20,30,40) );
+
+  Vector3 truth( -1, -1, -1 );
+
+  Vector3 translated = ComputeDEMTranslation( features, match );
+  ASSERT_EQ( truth, translated ) << "The translation is wrong.";
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
