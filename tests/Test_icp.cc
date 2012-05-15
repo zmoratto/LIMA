c0f1793e4cc37c4dd73ef93e46973876943d6151
rbeyer <rbeyer@1395e39a-8119-0410-9602-abf82d98ffd9>
2012-05-15 18:05:09 +0000

    Wrote a test for ComputeMatchingError3D().
    git-svn-id: https://babelfish.arc.nasa.gov/svn/stereopipeline/sandbox/lima@1772 1395e39a-8119-0410-9602-abf82d98ffd9

--- tests/Test_icp.cc
+++ tests/Test_icp.cc
@@ -24,24 +24,35 @@ TEST( ComputeDEMTranslation, works ){
   ASSERT_EQ( truth, translated ) << "The translation is wrong.";
 }
 
-TEST( ComputeMatchingError, works ){
-  vector<Vector3> features;
-  features.push_back( Vector3(1,1,1) );
-  features.push_back( Vector3(2,3,4) );
-  features.push_back( Vector3(21,31,41) );
+class ComputeMatchingError_Test : public ::testing::Test {
+  protected:
+  virtual void SetUp(){
+    features_.push_back( Vector3(1,1,1) );
+    features_.push_back( Vector3(2,3,4) );
+    features_.push_back( Vector3(21,31,41) );
+    match_.push_back( Vector3(0,0,0) );
+    match_.push_back( Vector3(1,2,3) );
+    match_.push_back( Vector3(20,30,40) );
+  }
 
-  vector<Vector3> match;
-  match.push_back( Vector3(0,0,0) );
-  match.push_back( Vector3(1,2,3) );
-  match.push_back( Vector3(20,30,40) );
+  virtual void TearDown() {}
 
+  vector<Vector3> features_;
+  vector<Vector3> match_;
+};
+
+TEST_F( ComputeMatchingError_Test, basic ){
   valarray<float> truth( sqrt(3), 3 );
-  
-  valarray<float> errors = ComputeMatchingError( features, match );
+  valarray<float> errors = ComputeMatchingError( features_, match_ );
 
   for( unsigned int i = 0; i < errors.size(); ++i ){
     EXPECT_NEAR( truth[i], errors[i], 0.001 ) << "Error array mismatch.";
   }
+}
+
+TEST_F( ComputeMatchingError_Test, 3D){
+  float error = ComputeMatchingError3D( features_, match_ );
+  ASSERT_NEAR( sqrt(3), error, 0.001 ) << "Incorrect error.";
 }
 
 int main(int argc, char **argv) {
