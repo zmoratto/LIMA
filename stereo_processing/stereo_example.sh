dd1e1c0eff8ea370cf8d98a5797a91b4fe64107c
anefian <anefian@1395e39a-8119-0410-9602-abf82d98ffd9>
2012-05-17 17:28:59 +0000

    added stereo calibration
    git-svn-id: https://babelfish.arc.nasa.gov/svn/stereopipeline/sandbox/lima@1788 1395e39a-8119-0410-9602-abf82d98ffd9

--- stereo_processing/stereo_example.sh
+++ stereo_processing/stereo_example.sh
@@ -7,8 +7,26 @@
 for leftImageFilename in /Users/anefian/projects/navigation/data/2012_0510_k10_marscape/*left*.pgm
 do
  rightImageFilename=${leftImageFilename/-left-/-right-} 
- echo $leftImageFilename
- echo $rightImageFilename
- ./stereo -a $leftImageFilename -b $rightImageFilename
+ #echo $leftImageFilename
+ #echo $rightImageFilename
+ #./stereo -a $leftImageFilename -b $rightImageFilename
+done
+
+
+#point clouds to dem/drg/vrml
+for stereoPair in `echo results/*`
+do
+    pc_txt=`echo $stereoPair/point*.txt`
+    echo $pc_txt
+    pc_tif=${pc_txt/.txt/-PC.tif}
+    echo $pc_tif
+    dem=${pc_txt/.txt/-DEM.tif} 
+    echo $dem
+    img_tif=${pc_txt/.txt/.tif}
+    echo $img_tif 
+    ../../../msl/scripts_msl/stereo $pc_txt $pc_tif
+    #../../../StereoPipeline/src/asp/Tools/point2mesh $pc_tif
+    ../../../StereoPipeline/src/asp/Tools/point2dem --mercator  -r mars $pc_tif -n --orthoimage $img_tif
+    hillshade $dem
 done
 
