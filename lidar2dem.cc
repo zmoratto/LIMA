32b0b049561cacfc605d1add4677d5357858e8e6
rbeyer <rbeyer@1395e39a-8119-0410-9602-abf82d98ffd9>
2012-05-29 23:07:20 +0000

    Streamlined and cleaned up the code.
    git-svn-id: https://babelfish.arc.nasa.gov/svn/stereopipeline/sandbox/lima@1808 1395e39a-8119-0410-9602-abf82d98ffd9

--- lidar2dem.cc
+++ lidar2dem.cc
@@ -4,31 +4,35 @@
 // All Rights Reserved.
 // __END_LICENSE__
 
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <vector>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <math.h>
+// #include <iostream>
+// #include <string>
+// #include <fstream>
+// #include <vector>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <math.h>
 
 //boost
+#include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/filesystem.hpp>
 #include <boost/operators.hpp>
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
 //VW
-#include <vw/Core.h>
-#include <vw/Image.h>
-#include <vw/FileIO.h>
-#include <vw/Cartography.h>
-#include <vw/Math.h>
-#include <vw/Math/Matrix.h>
+// #include <vw/Core.h>
+// #include <vw/Image.h>
+// #include <vw/FileIO.h>
+// #include <vw/Cartography.h>
+// #include <vw/Math.h>
+// #include <vw/Math/Matrix.h>
 
 //ATK
-#include "tracks.h"
 #include "coregister.h"
 #include "icp.h"
+#include "tracks.h"
 #include "util.h"
 
 using namespace std;
@@ -39,30 +43,30 @@ using namespace vw::cartography;
 int main( int argc, char *argv[] )
 {
  int verbose;
- std::string inputCSVFilename; 
+ //std::string inputCSVFilename; 
  std::string configFilename;
- std::string errorFilename;
+ //std::string errorFilename;
  std::vector<std::string> inputDEMFiles;
  std::string precDEMDir="NO_DIR";
- std::string resDir;
- std::string auxDir;
+ //std::string resDir;
+ //std::string auxDir;
 
  po::options_description general_options("Options");
  general_options.add_options()
-    ("Lidar-filename,l", po::value<std::string>(&inputCSVFilename))
+    ("lidar-filename,l", po::value<std::string>())
     ("inputDEMFiles,d", po::value<std::vector<std::string> >(&inputDEMFiles))
     ("DEM-precision-directory,p", 
      po::value<std::string>(&precDEMDir), 
 		"DEM precision directory.") 
     ("results-directory,r", 
-		po::value<std::string>(&resDir)->default_value("../results"), 
-		"results directory.") // Currently a no-op until we implement it below.
+		po::value<std::string>()->default_value("./results"), 
+		"results directory.") 
     ("settings-filename,s", 
 		po::value<std::string>(&configFilename)->default_value("lidar2dem_settings.txt"), 
 		"settings filename.")
-    ("error-filename,e", 
-		po::value<std::string>(&errorFilename)->default_value("lidar2dem_errors.txt"), 
-		"error output filename.")
+    ("errors,e", 
+		po::value<bool>()->zero_tokens(), 
+		"Write out detailed errors.")
     ("verbose,v", 
 		po::value<int>(&verbose)->default_value(1), 
 		"Verbosity level, zero emits no messages.")
@@ -82,35 +86,30 @@ int main( int argc, char *argv[] )
   usage << general_options << std::endl;
  
   po::variables_map vm;
-  try
-    {
-      po::store( po::command_line_parser( argc, argv ).options(options).positional(p).run(), vm );
-      po::notify( vm );
-    }
-  catch ( po::error &e )
-   {
-     std::cout << "An error occured while parsing command line arguments.\n";
-     std::cout << "\t" << e.what() << "\n\n";
-     std::cout << usage.str();
-     return 1;
-   }
+  try{
+    po::store( po::command_line_parser( argc, argv ).options(options).positional(p).run(), vm );
+    po::notify( vm );
+  }
+  catch( po::error& e ){
+    std::cout << "An error occured while parsing command line arguments." << endl;
+    std::cout << "\t" << e.what() << endl << endl;
+    std::cout << usage.str();
+    return 1;
+  }
   
   if( vm.count("help") )
     {
       std::cerr << usage.str() << std::endl;
-      return 1;
+      return 0;
     }
 
-  if (vm.count(inputCSVFilename)){
+  if( !vm.count("lidar-filename") ){
       std::cerr << "Error: Must specify at least  one Lidar file!" << std::endl << std::endl;
       std::cerr << usage.str();
       return 1;
   }
 
-  vector<string> DEMFiles; 
-  int numDEMInputFiles = inputDEMFiles.size();
-
-  if(numDEMInputFiles < 1) {
+  if( inputDEMFiles.empty() ) {
     std::cerr << "Error: Must specify at least  one DEM file!" << std::endl << std::endl;
     std::cerr << usage.str();
     return 1;
@@ -118,7 +117,7 @@ int main( int argc, char *argv[] )
 
   //determine if inputDEMFiles is a text file containing a list of DEM files, one DEM file or a set of DEM files
   //by reading the file extension. A text file containing the DEM list *must* have extension .txt 
-  DEMFiles = AccessDataFilesFromInput(inputDEMFiles);
+  vector<string> DEMFiles( AccessDataFilesFromInput(inputDEMFiles) );
  
   //Set up VW logging
   vw_log().console_log().rule_set().add_rule(vw::InfoMessage,"*");
@@ -133,78 +132,82 @@ int main( int argc, char *argv[] )
   }
 
   if( verbose > 0 ){ 
-   cout << settings << endl; 
+    cout << settings << endl;
+    if( DEMFiles.size() > 1 ){
+      string joined = boost::algorithm::join(DEMFiles, ",");
+      cout << DEMFiles.size() << " DEM files( " << joined << " )" << endl;
+    }
+    else{ cout << "DEM file: " << DEMFiles[0] << endl; }
   }
 
+  fs::path results_path( vm["results-directory"].as<string>() );
+  fs::path aux_path( results_path/"aux" );
 
-  int numDEMFiles = DEMFiles.size();
-  cout<<"numDEMFiles="<<numDEMFiles<<endl;
-
-  auxDir = resDir+"/aux";
-  cout<<"aux_dir="<<auxDir<<endl;
+  if( verbose > 0 ){ cout << "aux_path= " << aux_path << endl; }
   //create the results and auxiliary directories
-  string makeResDirCmd = "mkdir " + resDir;
-  string makeAuxDirCmd = "mkdir " + auxDir;
-  system(makeResDirCmd.c_str()); 
-  system(makeAuxDirCmd.c_str());
+  if( fs::create_directory( results_path ) == false ){
+    cerr << "Could not create the " << results_path << " directory." << endl;
+    return 1;
+  }
+  if( fs::create_directory( aux_path ) == false ){
+    cerr << "Could not create the " << aux_path << " directory." << endl;
+    return 1;
+  }
 
 
   //read LOLA tracks
   vector<vector<LOLAShot> > trackPts;
+  fs::path tracks_path( vm["lidar-filename"].as<string>() );
   try{
-    trackPts =  CSVFileRead(inputCSVFilename);
-    if( verbose > 0 ){ cout << "Tracks file: " << inputCSVFilename << endl; }
+    trackPts = LOLAFileRead( tracks_path.string() );
+    if( verbose > 0 ){ cout << "Tracks file: " << tracks_path.string() << endl; }
   }
-  catch (const vw::IOErr& error){
+  catch( const vw::IOErr& error ){
     cerr << error.what() << endl;
     exit(1);
   }
  
   //determine the overlapping DEMs - START
-  cout<<"Selecting the overlapping DEMs ..."<<endl;
+  cout << "Selecting the overlapping DEMs ..." << endl;
   std::vector<int> overlapIndices;
-  string overlapListFilename = auxDir+string("/")+GetFilenameNoPath(inputCSVFilename);
-  FindAndReplace(overlapListFilename, ".csv", "_overlap_list.txt"); 
-  int fileFound = ReadOverlapList(overlapListFilename, overlapIndices);
-  if (fileFound == 0){
-    Vector4 lat_lon_bb = FindMinMaxLat(trackPts); 
-    Vector4 lon_lat_bb;
-    lon_lat_bb[0]=lat_lon_bb[2];
-    lon_lat_bb[1]=lat_lon_bb[3];
-    lon_lat_bb[2]=lat_lon_bb[0];
-    lon_lat_bb[3]=lat_lon_bb[1];
-    //printf("lidar corners: %f %f %f %f\n", lon_lat_bb[0], lon_lat_bb[1], lon_lat_bb[2], lon_lat_bb[3]);
-    overlapIndices = makeOverlapListFromGeoTiff(DEMFiles, lon_lat_bb);
-    SaveOverlapList(overlapListFilename, overlapIndices);
+  fs::path overlapList_path( aux_path/tracks_path.filename().replace_extension("_overlap_list.txt") );
+  try{ 
+    overlapIndices = ReadVectorFrom<int>(overlapList_path);
+    if( !overlapIndices.empty() and overlapIndices[0] == -1 ){
+      // The previous generation of this code could have written a -1 to the file.
+      overlapIndices.clear();
+      }
   }
-  PrintOverlapList(overlapIndices);
-  int numOverlappingDEMs;
-  if ((overlapIndices.size() == 1) && (overlapIndices[0] == -1)){
-    numOverlappingDEMs = 0;
+  catch( const vw::ArgumentErr& error){
+    //Vector4 lat_lon_bb = FindMinMaxLat(trackPts); 
+    BBox2 bb = FindShotBounds( trackPts );
+    overlapIndices = makeOverlapList( DEMFiles, bb );
+    if( !overlapIndices.empty() ){
+      WriteVectorTo( overlapList_path, overlapIndices );
+    }
   }
-  else{
-     numOverlappingDEMs = (int)overlapIndices.size();
+  if( verbose > 0 ){
+    PrintOverlapList( overlapIndices );
+    cout << "done determining the overlapping DEMs." << endl;
   }
-  cout<<"numOverlapDEMs="<<numOverlappingDEMs<<endl;
-  cout<<"done determining the overlapping DEMs."<<endl;
   //determine the overlapping DEMs - END
-   
-  for (int index = 0; index < numOverlappingDEMs; index++){
-    
-    string inputDEMFilename = DEMFiles[overlapIndices[index]];
+ 
+  for( unsigned int index = 0; index < overlapIndices.size(); ++index ){
+    //string inputDEMFilename = DEMFiles[overlapIndices[index]];
+    fs::path inputDEM_path( DEMFiles[overlapIndices[index]] );
 
-    if (verbose > 0 ){ 
-      cout <<"DEM filename: " << inputDEMFilename << endl; 
+    if( verbose > 0 ){ 
+      cout << "DEM filename: " << inputDEM_path << endl; 
     }
     
     //read DEM file      
-    boost::shared_ptr<DiskImageResource> rsrc( new DiskImageResourceGDAL(inputDEMFilename) );
-    if (rsrc->has_nodata_read()){
-        settings.noDataVal = rsrc->nodata_read();
-	if( verbose > 0 ){
-	  cout 	<< "Found nodata value, " << settings.noDataVal 
-		<< ", in " << inputDEMFilename << endl;
-	}
+    boost::shared_ptr<DiskImageResource> rsrc( new DiskImageResourceGDAL( inputDEM_path.string() ) );
+    if( rsrc->has_nodata_read() ){
+      settings.noDataVal = rsrc->nodata_read();
+	  if( verbose > 0 ){
+        cout << "Found nodata value, " << settings.noDataVal 
+             << ", in " << inputDEM_path << endl;
+      }
     }
     else if( verbose > 0 ){
       cout << "Using default nodata value: " << settings.noDataVal << endl;
@@ -212,16 +215,16 @@ int main( int argc, char *argv[] )
     
     DiskImageView<float> DEM(rsrc);
     GeoReference DEMGeo;
-    read_georeference(DEMGeo, inputDEMFilename);
+    read_georeference(DEMGeo, inputDEM_path.string() );
 
-    if (precDEMDir.compare(string("NO_DIR"))!=0){
+    if( precDEMDir.compare(string("NO_DIR"))!=0 ){
       //select DEM points with high precision closest to LOLA tracks
-      string inputPrecFilename = GetFilenameNoPath(inputDEMFilename);
-      FindAndReplace(inputPrecFilename, string("dem"), string("Prec"));
-      inputPrecFilename = precDEMDir + string("/") + inputPrecFilename;
-      cout<<"DEMFilename="<<inputDEMFilename<<endl;
-      cout<<"PrecFilename="<<inputPrecFilename<<endl;
-      boost::shared_ptr<DiskImageResource> prec_rsrc( new DiskImageResourceGDAL(inputPrecFilename) );
+      fs::path precdir_path( precDEMDir );
+      fs::path prec_path( precdir_path/
+                          boost::algorithm::replace_all_copy(inputDEM_path.string(), "dem", "Prec") );
+      if( verbose > 0 ){ cout << "PrecFilename=" << prec_path << endl; }
+
+      boost::shared_ptr<DiskImageResource> prec_rsrc( new DiskImageResourceGDAL( prec_path.string() ) );
       DiskImageView<float> DEM_Prec(prec_rsrc);
       //select DEM points closest to LOLA tracks
       GetAllPtsFromDEM_Prec(trackPts, DEM, DEMGeo, settings.noDataVal, DEM_Prec);
@@ -233,45 +236,33 @@ int main( int argc, char *argv[] )
 
     Vector3 currTranslation;
     Matrix<float, 3,3 > currRotation;
+    currRotation.set_identity();
     vector<Vector3> xyzModelArray;//LOLA
     vector<Vector3> llrModelArray;//LOLA
     vector<Vector3> xyzMatchArray;//DEM
     valarray<float> xyzErrorArray;//error array same size as model and feature
     vector<float>   radErrorArray; //altitude-radial error array
     
-    currRotation[0][0] = 1.0;
-    currRotation[1][1] = 1.0;
-    currRotation[2][2] = 1.0;
-    
     //copy info to featureArray and modelArray
-    for(unsigned int k = 0; k < trackPts.size();k++){
-      for(unsigned int i = 0; i < trackPts[k].size(); i=i+settings.samplingStep(0)){
-	
-	if ((trackPts[k][i].valid == 1) && (trackPts[k][i].DEMPt[0].valid == 1)){
-	  Vector3 model;
-	
-	  model = trackPts[k][i].LOLAPt[2];
-	  model.z() *= 1000; // LOLA data is in km, DEMGeo is in m (for LROC DTMs).
+    for( unsigned int k = 0; k < trackPts.size(); ++k ){
+      for( unsigned int i = 0; i < trackPts[k].size(); i += settings.samplingStep(0) ){
+        if( (trackPts[k][i].valid == 1) && 
+            (trackPts[k][i].DEMPt[0].valid) &&
+            (trackPts[k][i].LOLAPt.size() > 2) ){
+          Vector3 model( trackPts[k][i].LOLAPt[2] );
+          model.z() *= 1000; // LOLA data is in km, DEMGeo is in m (for LROC DTMs).
           
-	  if ( ( trackPts[k][i].DEMPt[0].val > 0 ) &&
-           (model.x() <= 360) && (model.x() >= -180) && 
-           (model.y() <= 90) && (model.y() >= -90) && 
-           (model.z() > 1720000) && (model.z() < 1750000)){//this should go into the LOLA reader 
-	    if (verbose > 1){ 
-	      cout<<"altitude="<<model[2]<<", dem="<< trackPts[k][i].DEMPt[0].val<<endl; 
-	    }
-
-	    Vector3 xyzModel = lon_lat_radius_to_xyz(model);
-	    xyzModelArray.push_back(xyzModel);
-	    llrModelArray.push_back(model);
-            float radError = fabs(model[2]-1000*trackPts[k][i].DEMPt[0].val);
-	    radErrorArray.push_back(radError);
-            
-	  }
-	}
+          if( verbose > 1 ){ 
+            cout << "altitude=" << model.z() << ", dem=" << trackPts[k][i].DEMPt[0].val << endl; 
+          }
+          Vector3 xyzModel = lon_lat_radius_to_xyz(model);
+          xyzModelArray.push_back(xyzModel);
+          llrModelArray.push_back(model);
+          float radError = fabs(model[2]-1000*trackPts[k][i].DEMPt[0].val);
+          radErrorArray.push_back(radError);
+        }
       }
     }
-    
     xyzMatchArray.resize(xyzModelArray.size());
     xyzErrorArray.resize(xyzModelArray.size());
     
@@ -289,55 +280,52 @@ int main( int argc, char *argv[] )
     else{
       //run ICP-matching
       modelCentroid = find_centroid(xyzModelArray);
-      cout<<"modelCentroid="<<modelCentroid<<endl;
-      ICP_LIDAR_2_DEM(xyzMatchArray, DEM, DEMGeo, xyzModelArray, llrModelArray, settings, 
-		      currTranslation, currRotation, modelCentroid, xyzErrorArray);
+      cout << "modelCentroid=" << modelCentroid << endl;
+      ICP_LIDAR_2_DEM( xyzMatchArray, DEM, DEMGeo, xyzModelArray, llrModelArray, settings, 
+                       currTranslation, currRotation, modelCentroid, xyzErrorArray);
     }
-    string overlapDEMFileNoPathNoExt;
-    cout<<"inputDEMFilename="<<inputDEMFilename<<endl;
-    overlapDEMFileNoPathNoExt = GetFilenameNoExt(GetFilenameNoPath(inputDEMFilename));
+
+    //string overlapDEMFileNoPathNoExt;
+    //cout<<"inputDEMFilename="<<inputDEMFilename<<endl;
+    //overlapDEMFileNoPathNoExt = GetFilenameNoExt(GetFilenameNoPath(inputDEMFilename));
+    //overlapDEMFileNoPathNoExt = inputDEM_path.filename().stem();
     
-    string LOLAFilenameNoPathNoExt;
-    cout<<"inputCSVFilename="<<inputCSVFilename<<endl;
-    LOLAFilenameNoPathNoExt = GetFilenameNoExt(GetFilenameNoPath(inputCSVFilename));
-    cout<<"LOLAFilenameNoPathNoExt="<<LOLAFilenameNoPathNoExt<<endl;
-    cout<<"overlapDEMFileNoPathNoExt="<<overlapDEMFileNoPathNoExt<<endl;
-    string statsFilename;
+    //string LOLAFilenameNoPathNoExt;
+    //cout<<"inputCSVFilename="<<inputCSVFilename<<endl;
+    //LOLAFilenameNoPathNoExt = GetFilenameNoExt(GetFilenameNoPath(inputCSVFilename));
+    //LOLAFilenameNoPathNoExt = tracks_path.filename().stem();
+    //cout<<"LOLAFilenameNoPathNoExt="<<LOLAFilenameNoPathNoExt<<endl;
+    //cout<<"overlapDEMFileNoPathNoExt="<<overlapDEMFileNoPathNoExt<<endl;
+    //string statsFilename;
     
-    statsFilename = resDir + "/" + LOLAFilenameNoPathNoExt + "_" + overlapDEMFileNoPathNoExt + "_stats.txt";
-    errorFilename = resDir + "/" + LOLAFilenameNoPathNoExt + "_" + overlapDEMFileNoPathNoExt + "_error.txt";
-    
-    if( vm.count("error-filename") && (xyzModelArray.size() >0 )){
+    //statsFilename = resDir + "/" + LOLAFilenameNoPathNoExt + "_" + overlapDEMFileNoPathNoExt + "_stats.txt";
+    ostringstream os;
+    os << tracks_path.filename().stem().string() << "_" << inputDEM_path.filename().stem().string();
+    fs::path stats_path( results_path/os.str().append("_stats.txt") );
+
+    //errorFilename = resDir + "/" + LOLAFilenameNoPathNoExt + "_" + overlapDEMFileNoPathNoExt + "_error.txt";
+    if( vm.count("errors") && !xyzModelArray.empty() ){
+      fs::path error_path( results_path/os.str().append("_error.txt") );
       vector<string> titles(4);
       titles[0] = "Latitude";
       titles[1] = "Longitude";
       titles[2] = "Radius (m)";
       titles[3] = "Errors";  
-      SaveDEMErrors( errorFilename, llrModelArray, xyzErrorArray, titles );
+      SaveDEMErrors( error_path.string(), llrModelArray, xyzErrorArray, titles );
        
-      vector<float> histBins;
-      int numBins = 15;
-      histBins.resize(numBins);
-      histBins[0] = 0;
-      histBins[1] = 25;
-      histBins[2] = 50; 
-      histBins[3] = 75;
-      histBins[4] = 100;
-      histBins[5] = 125;
-      histBins[6] = 150; 
-      histBins[7] = 175;
-      histBins[8] = 200;
-      histBins[9] = 225;
-      histBins[10] = 250; 
+      vector<float> histBins( 15 );
+      for( unsigned int i = 0; i < 11; ++i ){
+        histBins[i] = i * 25;
+      }
       histBins[11] = 1000;
       histBins[12] = 2500;
       histBins[13] = 5000;
       histBins[14] = 10000; 
     
-      SaveStatistics (statsFilename, /*xyzErrorArray*/radErrorArray, histBins);
+      SaveStatistics( stats_path.string(), radErrorArray, histBins );
     }
     
-    if(( verbose >= 0 ) && (xyzModelArray.size() > 0)){ 
+    if( ( verbose >= 0 ) && !xyzModelArray.empty() ){ 
 
        /*
        //this must be changed - START
@@ -377,17 +365,17 @@ int main( int argc, char *argv[] )
       Matrix<double> f_centered(xyzMatchArray.size(),3);
       Matrix<double> m_centered(xyzMatchArray.size(),3);
       
-      for( unsigned int i = 0; i < xyzMatchArray.size(); i++ ){
-	select_row(f_centered,i) = xyzMatchArray[i] - f_cent;
-	select_row(m_centered,i) = xyzModelArray[i] - modelCentroid;
+      for( unsigned int i = 0; i < xyzMatchArray.size(); ++i ){
+        select_row(f_centered,i) = xyzMatchArray[i] - f_cent;
+        select_row(m_centered,i) = xyzModelArray[i] - modelCentroid;
       }
       
       Matrix<double> ratio(xyzMatchArray.size(),3);
       Vector3 scale;
       
       ratio = elem_quot( f_centered * transpose(currRotation) , m_centered );
-      for( unsigned int i = 0; i<=2; i++ ){
-	scale(i) = sum( select_col(ratio,i) ) / ratio.rows();
+      for( unsigned int i = 0; i<=2; ++i ){
+        scale(i) = sum( select_col(ratio,i) ) / ratio.rows();
       }
       
       cout << endl
