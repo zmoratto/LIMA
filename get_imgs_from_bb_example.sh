d6be2d96895abc3f731069ba324a4edeea4f90ce
anefian <anefian@1395e39a-8119-0410-9602-abf82d98ffd9>
2012-09-05 17:37:29 +0000

    delete get_imgs_from_bb_example.sh
    git-svn-id: https://babelfish.arc.nasa.gov/svn/stereopipeline/sandbox/lima@2067 1395e39a-8119-0410-9602-abf82d98ffd9

--- get_imgs_from_bb_example.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-
-
-Apollo_DEM=`ls -d -1 /Users/anefian/projects/LMMP/LMMP_delivery/2011_1102_Apollo_DEM/*.tif`
-#run for all geotiifs at once
-./get_imgs_from_bb -n 30 -s -30 -e -50 -w 50 $Apollo_DEM
-
